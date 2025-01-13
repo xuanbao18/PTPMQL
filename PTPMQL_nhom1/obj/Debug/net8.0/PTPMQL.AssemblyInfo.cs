56bcce44b618b9167eb1738235d9f111c700bc8a
2121050486 <2121050486@student.humg.edu.vn>
2025-01-13 09:38:57 +0700

    BTH_BTHHTML

--- PTPMQL_nhom1/obj/Debug/net8.0/PTPMQL.AssemblyInfo.cs
+++ PTPMQL_nhom1/obj/Debug/net8.0/PTPMQL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PTPMQL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d49f3e6bbfa9879537f25c8b6a62ac156d405284")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+128373eb19c4f38ef09b3bc0f223a468f268288e")]
 [assembly: System.Reflection.AssemblyProductAttribute("PTPMQL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PTPMQL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
