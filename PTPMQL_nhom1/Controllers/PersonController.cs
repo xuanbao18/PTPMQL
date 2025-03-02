c5a8d0d9bf80972ebbf50341077f77c613d790ee
2121050486 <2121050486@student.humg.edu.vn>
2025-03-02 22:27:08 +0700

    thuchanh

--- PTPMQL_nhom1/Controllers/PersonController.cs
+++ PTPMQL_nhom1/Controllers/PersonController.cs
@@ -1,19 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using PTPMQL.Data;
 using PTPMQL.Models;
+
 
 namespace PTPMQL.Controllers
 {
     public class PersonController : Controller
+    
     {
         private readonly ApplicationDbcontext _context;
+        private object await_context;
+
         public PersonController(ApplicationDbcontext context)
         {
             _context = context;
         }
         public async Task<IActionResult> Index()
         {
-            var model = await _context.Person.ToListAsync();
+            var model = await _context.Persons.ToListAsync();
             return View(model);
         }
         public IActionResult Create()
@@ -32,21 +37,93 @@ namespace PTPMQL.Controllers
             }
             return View(person);
         }
-        public async <IActionResult> Edit (string ID)
+        public async Task<IActionResult> Edit (string ID)
         {
             if (ID == null || _context.Person == null)
             {
                 return NotFound();
             } 
-            var person = await _context.Person.FindAsync(ID);
+
+            var person = await _context.Persons.FindAsync(ID);
             if (person == null)
+            { 
+                return NotFound();
+            }
+            return View(person);
         }
-        public IActionResult Index()
-    {
-        return View();
-    }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string ID, [Bind("PersonID,FullName,Address")] Person person)
+        {
+            if (ID != person.PersonID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(person);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PersonExists(person.PersonID) )
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(person);
+        }
+        public async Task<IActionResult> Delete(string ID)
+        {
+            if (ID ==null || _context.Person == null)
+            {
+                return NotFound();
+            }
+
+            var person = await _context.Persons
+                .FirstOrDefaultAsync(m => m.PersonID == ID);
+            if (person == person)
+            {
+                return NotFound();
+            }
+
+            return View(person);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string ID)
+        {
+            if (_context.Person == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Person' is null."); 
+            }
+            var person = await _context.Persons.FindAsync(ID);
+            if (person != null)
+            {
+                _context.Persons.Remove(person);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        private bool PersonExists(string ID)
+        {
+            return (_context.Persons?.Any(e => e.PersonID == ID)).GetValueOrDefault();
+        }
+
+     
     [HttpPost]
     public IActionResult Index(Person ps)
+    
     {
         string str0utput ="xin chao" + ps.PersonID + "-"+ ps.FullName+"-"+ps.Address;
         ViewBag.infoPerson = str0utput;
--- PTPMQL_nhom1/obj/Debug/net8.0/PTPMQL.AssemblyInfo.cs
+++ PTPMQL_nhom1/obj/Debug/net8.0/PTPMQL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PTPMQL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5743a64b1855d8997899c468f6aeb2cb45086664")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a92b70904bf594302ac755a53d23194ff274d61")]
 [assembly: System.Reflection.AssemblyProductAttribute("PTPMQL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PTPMQL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
